5bdee6f7fbbe1e89f9703a208718125d9b9b3990
Amaury Deltenre <amaury.deltenre@gmail.com>
2020-02-06 10:21:45 +0100

    push many steps

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+let givenWord = "BONJOUR";
+let word = givenWord.toLowerCase();
+let guessedWord = word.split("");
+let guess = [];
+for (let i = 0; i < guessedWord.length; i++) {
+  guess[i] = "_";
+}
+let leftToGuess = guessedWord.length;
+let count = 0;
+
+document.getElementById("run").addEventListener("click", () => {
+  while (leftToGuess > 0) {
+    function guessLetter() {
+      let givenLetter = prompt("Guess a letter");
+      let letter = givenLetter.toLowerCase();
+      let match = false;
+      for (let j = 0; j < guessedWord.length; j++) {
+        if (letter === guessedWord[j]) {
+          alert("Congrats! You found one!");
+          guess[j] = letter;
+          match = true;
+          leftToGuess--;
+        } else if (j == guessedWord.length - 1 && match == false) {
+          alert("Not the good one, try again!");
+        }
+      }
+    }
+
+    guessLetter();
+    count++;
+    if (leftToGuess > 0) {
+      alert(
+        "This is your result for now: " +
+          guess.join(" ") +
+          "\nnumber of trials: " +
+          count
+      );
+    } else {
+      alert("Congrats, you found the word " + givenWord + " correctly!");
+    }
+    console.log(guessedWord);
+    console.log(guess);
+  }
+});
